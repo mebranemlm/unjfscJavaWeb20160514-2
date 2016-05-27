e1c6e2d192008963aab866d6cf4eddd0cdb5ad6b
aedi <none>
2016-05-27 02:30:43 -0500

    UsuarioDAOL

---
+++ src/daol/UsuarioDAOL.java
@@ -0,0 +1,18 @@
+package daol;
+
+import java.util.List;
+
+import model.Usuario;
+
+public interface UsuarioDAOL 
+{
+	//Metodo para validarLogin, con parametro de entrada xxxxx
+	public abstract Usuario validateUsua(Usuario obj) throws Exception;
+	public abstract Usuario questUsua(Usuario obj) throws Exception;
+	public abstract void addUsua(Usuario obj) throws Exception;
+	public abstract void editUsua(Usuario obj) throws Exception;
+	public abstract List<Usuario> questListUsua(Usuario obj) throws Exception;
+	//elmininar
+	//registrar
+	//...ncosas	
+}
